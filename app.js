60b4dfbb9298a004529fad480ce74cce752720a7
Maria Camila Esquivel Alvarez <116690768+mariacamilaea@users.noreply.github.com>
2022-12-10 09:34:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {db} from './mongodb.js';
+import { shevento } from './controllers/eventcontrollers.js';
+import { showuser } from './controllers/userscontroller.js';
+
+const router= express.Router();
+const app = express();
+
+app.use('/',router);
+
+router.get('/eventos',shevento)
+
+router.get('/usuarios',showuser)
+
+router.get('/', (req,res) => {
+    res.send("🚀😎 Deploy en Heroku🚀🚀")
+})
+
+var PORT = process.env.PORT || 8000;
+app.listen(PORT, ()=> {
+    console.log(`Conectado en el servidor: http://localhost:${PORT}`)
+})
